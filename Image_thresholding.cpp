4f8c70e2a4bc4a31458afa632fa9c38fbe66400b
Aksh-Patel <87661032+Aksh2107@users.noreply.github.com>
2022-12-19 13:07:57 +0000

    Final commit

--- Image_thresholding.cpp
+++ Image_thresholding.cpp
@@ -3,7 +3,7 @@
 
 int main (int argc, char* argv[])
 {
-        cv::Mat h_img1 = cv::imread("images/cameraman.tif", 0);
+        cv::Mat h_img1 = cv::imread("input_image.tif", 0);
         //Define device variables
         cv::cuda::GpuMat d_result1,d_result2,d_result3,d_result4,d_result5, d_img1;
         //Upload image on device
