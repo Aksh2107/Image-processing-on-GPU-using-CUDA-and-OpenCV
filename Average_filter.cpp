be1a6238b11752549774fa3b463382e3402c1bff
Aksh-Patel <87661032+Aksh2107@users.noreply.github.com>
2022-12-19 13:00:34 +0000

    Final Commit

--- Average_filter.cpp
+++ Average_filter.cpp
@@ -4,7 +4,7 @@
 
 int main ()
 {
-    cv::Mat h_img1 = cv::imread("images/cameraman.tif",0);
+    cv::Mat h_img1 = cv::imread("input_image.tif",0);
     cv::cuda::GpuMat d_img1,d_result3x3,d_result5x5,d_result7x7;
  
 	d_img1.upload(h_img1);
